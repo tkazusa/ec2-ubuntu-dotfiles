6e39e2fed718f6d828614169eb08c46f4f437ecd
Ubuntu <ubuntu@ip-172-31-84-216.ec2.internal>
2022-04-13 01:35:37 +0000

    deeplearning-ami

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@ DOTDIR=$HOME/.dotfiles
 # --------------------
 # Install packages
 # --------------------
-source $DOTDIR/install.d/apt.sh
-source $DOTDIR/install.d/install-nodejs.sh
-source $DOTDIR/install.d/install-rust.sh
-source $DOTDIR/install.d/install-awscli2.sh
-source $DOTDIR/install.d/install-docker.sh
-source $DOTDIR/install.d/install-cdk.sh
+# source $DOTDIR/install.d/apt.sh
+# source $DOTDIR/install.d/install-nodejs.sh
+# source $DOTDIR/install.d/install-rust.sh
+# source $DOTDIR/install.d/install-awscli2.sh
+# source $DOTDIR/install.d/install-docker.sh
+# source $DOTDIR/install.d/install-cdk.sh
 source $DOTDIR/install.d/pip.sh
 source $DOTDIR/install.d/install-fish.sh
-source $DOTDIR/install.d/install-amplify.sh
+# source $DOTDIR/install.d/install-amplify.sh
 source $DOTDIR/install.d/link.sh
 
 # -------------------
